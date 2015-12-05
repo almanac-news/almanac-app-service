1216b65011aec97be03c1f40b0111a0d42a4f1a6
Nathan Levine <natejlevine@gmail.com>
2015-12-04 20:05:57 -0800

    Added api endpoint /news which returns json with the 20 newest NYT articles' title, abstract, and URL

--- server/app.py
+++ server/app.py
@@ -1,24 +1,44 @@
 from flask import Flask
+from flask_restful import Resource, Api
 import requests
+import unicodedata
+
+app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
+def normalize(unicode):
+    return unicodedata.normalize('NFKD', unicode).encode('ascii', 'ignore')
+
 def extractArticles(obj):
-    return {'title': obj['title'], 'abstract': obj['abstract'], 'url': obj['url']}
+    return {'title': normalize(obj['title']), 'abstract': normalize(obj['abstract']), 'url': normalize(obj['url'])}
+#
+# @app.route("/proxy-example")
+# def proxy_example():
+#     uri = "http://api.nytimes.com/svc/news/v3/content/all/all/2?api-key=202f0d73b368cec23b977f5a141728ce:17:73664181"
+#     # params = {'api-key': ''}
+#     r = requests.get(uri)
+#     objectResp = r.json()
+#
+#     articles = map(extractArticles, objectResp["results"])
+#     print articles
+#
+#     return 'hello world'
 
-@app.route("/proxy-example")
-def proxy_example():
-    uri = "http://api.nytimes.com/svc/news/v3/content/all/all/2?api-key=202f0d73b368cec23b977f5a141728ce:17:73664181"
-    # params = {'api-key': ''}
-    r = requests.get(uri)
-    objectResp = r.json()
+class GetNewswire(Resource):
+    def get(self):
+        uri = "http://api.nytimes.com/svc/news/v3/content/all/all/2?api-key=202f0d73b368cec23b977f5a141728ce:17:73664181"
+        # params = {'api-key': ''}
+        r = requests.get(uri)
+        objectResp = r.json()
+        articles = map(extractArticles, objectResp["results"])
 
-    articles = map(extractArticles, objectResp["results"])
-    print articles
+        return articles
 
-    return 'hello world'
+api.add_resource(GetNewswire, '/news')
 
 if __name__ == '__main__':
     app.run()

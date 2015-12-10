973725eebe438de15136ebb5b27b162835d21d84
Soroush Mehraein <soroushmehraein@gmail.com>
2015-12-09 22:45:42 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 # deploy.sh
 #! /bin/bash
 
+aws configure set aws_access_key_id $AWSKEY
+aws configure set aws_secret_access_key $AWSSECRETKEY
+aws configure set default.region us-west-2
+aws configure set default.output json
+
 SHA1=$1
 
 # Deploy image to Docker Hub

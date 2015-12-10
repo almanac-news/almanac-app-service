1d2792cda15e301f9d99319e8d12a5296d58272a
Nathan Levine <natejlevine@gmail.com>
2015-12-09 16:49:10 -0800

    removed port setting

--- server/app.py
+++ server/app.py
@@ -94,4 +94,4 @@ api.add_resource(GetTop, '/top/<category>')
 api.add_resource(GetFinData, '/date/<date>')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0')

4e8fd42269b9c085b4b9f7f2feb20d697cbbd0f9
Nathan Levine <natejlevine@gmail.com>
2015-12-28 15:17:15 -0800

    fix bug in sleep function

--- server/app.py
+++ server/app.py
@@ -48,43 +48,44 @@ def normalize(unicode):
 
 #Function to scrape article text, clean and format it, and store it into redis under a hashmap
 def extractArticles(obj):
-    #setup redis connection
-    rs = redis.StrictRedis(host='data-cache', port=6379, db=0)
-    #format long-url in 'url' formatting
-    url = urllib.quote(obj['url'], safe='')
-    access_token = 'ab6dbf0df548c91cffaa1ae82e0d9f4a52dfe4f8'
-    #query bitly with long-url to get shortened version
-    bitly_uri = 'https://api-ssl.bitly.com//v3/shorten?access_token=' + access_token + '&longUrl=' + url + '&format=txt'
-    rq = requests.get(bitly_uri)
+    categories = ['World', 'U.S.', 'Politics', 'Business Day', 'Technology', 'Science', 'Health', 'Real Estate']
+    if obj["section"] in categories:
+        #setup redis connection
+        rs = redis.StrictRedis(host='localhost', port=6379, db=0)
+        #format long-url in 'url' formatting
+        url = urllib.quote(obj['url'], safe='')
+        access_token = 'ab6dbf0df548c91cffaa1ae82e0d9f4a52dfe4f8'
+        #query bitly with long-url to get shortened version
+        bitly_uri = 'https://api-ssl.bitly.com//v3/shorten?access_token=' + access_token + '&longUrl=' + url + '&format=txt'
+        rq = requests.get(bitly_uri)
 
-    key = rq.text[-8:-1]
+        key = rq.text[-8:-1]
 
-    #if article is not already in redis db
-    if (rs.exists(key) == 0):
-        #scrape the news article
-        html = br.open(obj['url']).read()
+        #if article is not already in redis db
+        if (rs.exists(key) == 0):
+            #scrape the news article
+            html = br.open(obj['url']).read()
 
-        #run the article through readability
-        readable_article = Document(html).summary()
-        readable_title = Document(html).title()
+            #run the article through readability
+            readable_article = Document(html).summary()
+            readable_title = Document(html).title()
 
-        #run it through BeautifulSoup to parse only relevant tags
-        soup = BeautifulSoup(readable_article, 'lxml')
+            #run it through BeautifulSoup to parse only relevant tags
+            soup = BeautifulSoup(readable_article, 'lxml')
+            #grab story content tags and concat them together
+            storycontent = (soup.find_all("p", { "class":"story-content" }))
+            encodedFinal = reduce(lambda x, y: x + '<p>' + y.text + '</p>', storycontent, '')
+            article = {'title': normalize(obj['title']), 'abstract': normalize(obj['abstract']), 'url': rq.text[0:-1], 'created_date': obj['created_date'][0:10], 'article_text': encodedFinal}
 
-        #grab story content tags and concat them together
-        storycontent = (soup.find_all("p", { "class":"story-content" }))
-        encodedFinal = reduce(lambda x, y: x + '<p>' + y.text + '</p>', storycontent, '')
-        article = {'title': normalize(obj['title']), 'abstract': normalize(obj['abstract']), 'url': rq.text[0:-1], 'created_date': obj['created_date'][0:10], 'article_text': encodedFinal}
-
-        rs.hmset(key, article)
-        rs.expire(key, 3600)
-	r.db('test').table('news').wait()
-	r.db('test').table('news').insert({'id': key, 'article': article}).run(conn)
+            rs.hmset(key, article)
+            rs.expire(key, 3600)
+        r.db('test').table('news').wait()
+    	r.db('test').table('news').insert({'id': key, 'article': article}).run(conn)
 
 def delOldData():
     #worker running every 72 hours to delete the last market period's data (390 records)
     #(currently every 1 hr and deleting 120 records for testing)
-    rs = redis.StrictRedis(host='data-cache', port=6379, db=1)
+    rs = redis.StrictRedis(host='localhost', port=6379, db=1)
     keys = rs.keys('*')
     #for each stock ticker
     for key in keys:
@@ -117,7 +118,7 @@ def getNews():
 
 def getFinData():
     #setup redis connection
-    rs = redis.StrictRedis(host='data-cache', port=6379, db=1)
+    rs = redis.StrictRedis(host='localhost', port=6379, db=1)
     url = "https://query.yahooapis.com/v1/public/yql?q=select%20symbol%2C%20LastTradePriceOnly%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22MCHI%22%2C%0A%22DBA%22%2C%0A%22USO%22%2C%0A%22IYZ%22%2C%0A%22EEM%22%2C%0A%22VPL%22%2C%0A%22XLE%22%2C%0A%22HEDJ%22%2C%0A%22XLF%22%2C%0A%22XLV%22%2C%0A%22EPI%22%2C%0A%22XLI%22%2C%0A%22EWJ%22%2C%0A%22ILF%22%2C%0A%22BLV%22%2C%0A%22UDN%22%2C%0A%22XLB%22%2C%0A%22IYR%22%2C%0A%22SCPB%22%2C%0A%22FXE%22%2C%0A%22IWM%22%2C%0A%22XLK%22%2C%0A%22XLU%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
     rq = requests.get(url)
     time = rq.json()["query"]["created"]
@@ -132,7 +133,7 @@ def populateNews():
     next_call = time.time()
     while True:
         getNews()
-        next_call = next_call+60
+        next_call = next_call+120
         time.sleep(next_call - time.time())
 
 def populateFinData():
@@ -170,6 +171,13 @@ def delWorker():
         #wait 1 hour
         next_call = next_call+3600
         time.sleep(next_call - time.time())
+
+p = HTMLParser.HTMLParser()
+resp = requests.get("http://api.nytimes.com/svc/news/v3/content/all/all/24?limit=100&api-key=202f0d73b368cec23b977f5a141728ce:17:73664181")
+objectResp = json.loads(p.unescape(resp.text))
+#pull out relevant information only
+for obj in objectResp["results"]:
+    extractArticles(obj)
 
 newsThread = threading.Thread(target=populateNews)
 dataThread = threading.Thread(target=populateFinData)
